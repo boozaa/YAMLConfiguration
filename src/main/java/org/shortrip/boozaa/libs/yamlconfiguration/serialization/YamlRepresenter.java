d78cd6e7acffbe18cdb218c9e8c91b3d22a00292
boozaa <boozaa@shortrip.org>
2013-05-25 23:29:48 +0200

    Initial
    Initial commit

---
+++ src/main/java/org/shortrip/boozaa/libs/yamlconfiguration/serialization/YamlRepresenter.java
@@ -0,0 +1,38 @@
+package org.shortrip.boozaa.libs.yamlconfiguration.serialization;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.shortrip.boozaa.libs.yamlconfiguration.ConfigurationSection;
+import org.shortrip.boozaa.libs.yamlconfiguration.ConfigurationSerializable;
+import org.shortrip.boozaa.libs.yamlconfiguration.ConfigurationSerialization;
+import org.yaml.snakeyaml.nodes.Node;
+import org.yaml.snakeyaml.representer.Representer;
+
+
+public class YamlRepresenter extends Representer {
+
+    public YamlRepresenter() {
+        this.multiRepresenters.put(ConfigurationSection.class, new RepresentConfigurationSection());
+        this.multiRepresenters.put(ConfigurationSerializable.class, new RepresentConfigurationSerializable());
+    }
+
+    private class RepresentConfigurationSection extends RepresentMap {
+        @Override
+        public Node representData(Object data) {
+            return super.representData(((ConfigurationSection) data).getValues(false));
+        }
+    }
+
+    private class RepresentConfigurationSerializable extends RepresentMap {
+        @Override
+        public Node representData(Object data) {
+            ConfigurationSerializable serializable = (ConfigurationSerializable) data;
+            Map<String, Object> values = new LinkedHashMap<String, Object>();
+            values.put(ConfigurationSerialization.SERIALIZED_TYPE_KEY, ConfigurationSerialization.getAlias(serializable.getClass()));
+            values.putAll(serializable.serialize());
+
+            return super.representData(values);
+        }
+    }
+}
